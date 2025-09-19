2ecf58cd8944dbf50051e096fff4ae346ffae9a9
SapryWenInera <97859194+SapryWenInera@users.noreply.github.com>
2025-09-19 18:01:42 -0300

    Add initial Backend impl to RocksDBWrapper

--- src/polodb_core/db/backend.rs
+++ src/polodb_core/db/backend.rs
@@ -19,3 +19,23 @@ where
     where
         P: AsRef<Path>;
 }
+
+impl Backend for RocksDBWrapper {
+    type ReadTransaction = RocksDBTransaction;
+    type WriteTransaction = RocksDBTransaction;
+
+    fn open_path<P>(path: P) -> Result<Self>
+    where
+        P: AsRef<Path>,
+    {
+        Ok(Self::open(path.as_ref())?)
+    }
+
+    fn write_transaction(&self) -> Result<Self::WriteTransaction> {
+        Ok(self.begin_transaction()?)
+    }
+
+    fn transaction(&self) -> Result<Self::ReadTransaction> {
+        todo!()
+    }
+}
